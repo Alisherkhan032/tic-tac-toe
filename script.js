85cb87494adfb54f77a2651a01abb5adfbd893f0
alisherkhann <sherkhanali032@gmail.com>
2024-10-30 18:12:49 +0530

    added color to boxes after winning

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let boxes = document.querySelectorAll('.box');
 let resetBtn = document.querySelector('.reset-btn');
 let output = document.querySelector('.outputFeild');
-console.log(output)
 
 let flag_X = true;
 let totalMovesPlayed = 0;
+let defaultColor = 'bg-[#205f5b]';
+let WinnerColor = 'bg-pink-800';
+let hoverColor = 'hover:bg-[#004643d3]';
 
 let winningPattern = [
     [0,1,2],
@@ -22,6 +24,11 @@ resetBtn.addEventListener('click',()=>{
     flag_X = true;
     totalMovesPlayed = 0;
     output.innerHTML = ''
+    for(let i=0; i<9;i++){
+        boxes[i].classList.remove(WinnerColor);
+        boxes[i].classList.add(defaultColor);
+        boxes[i].classList.add(hoverColor);
+    }
 })
 
 boxes.forEach(box => {
@@ -54,7 +61,8 @@ let checkWinner = (box)=>{
         if(posVal1 != "" && posVal2 != "" && posVal3 != ""){
             if(posVal1 == posVal2 && posVal2 == posVal3){
                 output.innerHTML = `Winner is ${posVal1}`
-                console.log("The winner is found ",posVal1);
+                console.log('pattern is ',pattern)
+                colorBoxes(pattern);
                 disableButtons();
                 return true;
             }
@@ -72,5 +80,19 @@ let enableButtonsAndResetState = ()=>{
         box.disabled = false;
         box.innerHTML = '';
     })
+}
+
+let colorBoxes = (pattern)=>{
+    for (const element of pattern) {
+        console.log(element)
+        boxes[element].classList.remove(defaultColor)
+        boxes[element].classList.add(WinnerColor);
+
+        const currentClasses = boxes[element].className.split(' ');
+        const filteredClasses = currentClasses.filter(cls => !cls.startsWith('hover:'));
+
+        // Set the new class name with the filtered classes
+        boxes[element].className = filteredClasses.join(' ');
+    }
 }
 
